329fa1d64f722b7ff243fad87580a58f5a23645f
Abhimanyu2112 <abhimanyumurkumbi@gmail.com>
2026-09-25 12:39:07 -0400

    csv file read and write

--- models.py
+++ models.py
@@ -1,2 +1,14 @@
 
-print("Hello World!")
+import csv
+
+with open("students.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Name", "Age", "Grade"])
+    writer.writerow(["John", 15, "10th"])
+    writer.writerow(["Alice", 14, "9th"])
+    writer.writerow(["Bob", 16, "11th"])
+
+with open("students.csv", "r", newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
